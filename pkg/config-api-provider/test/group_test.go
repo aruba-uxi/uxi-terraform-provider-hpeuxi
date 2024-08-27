d3761d484757a1fb9599bceae38d137bcdd9e541
Alistair Yan <52126234+1riatsila1@users.noreply.github.com>
2024-08-27 16:10:01 +0200

    fix: group resource test and alignment (#11)

--- pkg/config-api-provider/test/group_test.go
+++ pkg/config-api-provider/test/group_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"testing"
 
+	"github.com/aruba-uxi/configuration-api-terraform-provider/pkg/terraform-provider-configuration/provider/resources"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
@@ -11,25 +12,115 @@ type Fetcher interface {
 }
 
 func TestGroupResource(t *testing.T) {
-	createGroupConfig := `
-		resource "uxi_group" "test_group" {
-		  name       = "temporary_name"
-		  parent_uid = "temporary_parent_uid"
-		}`
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: providerConfig + createGroupConfig,
+				PreConfig: func() {
+					response := resources.GroupResponseModel{
+						UID:       "uid",
+						Name:      "name",
+						ParentUid: "parent_uid",
+						Path:      "parent_uid.uid",
+					}
+					resources.CreateGroup = func(request resources.GroupCreateRequestModel) resources.GroupResponseModel {
+						return response
+					}
+					resources.GetGroup = func() resources.GroupResponseModel {
+						return response
+					}
+				},
+				Config: providerConfig + `
+				resource "uxi_group" "my_group" {
+					name       = "name"
+					parent_uid = "parent_uid"
+				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("uxi_group.test_group", "name", "temporary_name"),
-					resource.TestCheckResourceAttr("uxi_group.test_group", "parent_uid", "temporary_parent_uid"),
-					resource.TestCheckResourceAttrSet("uxi_group.test_group", "id"),
-					resource.TestCheckResourceAttrSet("uxi_group.test_group", "last_updated"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "name", "name"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "parent_uid", "parent_uid"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "id", "uid"),
 				),
 			},
+			// ImportState testing
+			{
+				ResourceName:      "uxi_group.my_group",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update that does not trigger a recreate
+			{
+				PreConfig: func() {
+					response := resources.GroupResponseModel{
+						UID:       "uid",
+						Name:      "updated_name",
+						ParentUid: "parent_uid",
+						Path:      "parent_uid.uid",
+					}
+					resources.UpdateGroup = func(request resources.GroupUpdateRequestModel) resources.GroupResponseModel {
+						return response
+					}
+					resources.GetGroup = func() resources.GroupResponseModel {
+						return response
+					}
+				},
+				Config: providerConfig + `
+					resource "uxi_group" "my_group" {
+						name       = "updated_name"
+						parent_uid = "parent_uid"
+					}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("uxi_group.my_group", "name", "updated_name"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "parent_uid", "parent_uid"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "id", "uid"),
+				),
+				Destroy: false,
+			},
+			// Update that does trigger a recreate
+			{
+				PreConfig: func() {
+					response := resources.GroupResponseModel{
+						UID:       "new_uid",
+						Name:      "name",
+						ParentUid: "updated_parent_uid",
+						Path:      "updated_parent_uid.uid",
+					}
+					resources.CreateGroup = func(request resources.GroupCreateRequestModel) resources.GroupResponseModel {
+						return response
+					}
+					called := false
+					resources.GetGroup = func() resources.GroupResponseModel {
+						if !called {
+							called = true
+							return resources.GroupResponseModel{
+								UID:       "uid",
+								Name:      "name",
+								ParentUid: "parent_uid",
+								Path:      "parent_uid.uid",
+							}
+						} else {
+							return resources.GroupResponseModel{
+								UID:       "new_uid",
+								Name:      "name",
+								ParentUid: "updated_parent_uid",
+								Path:      "updated_parent_uid.uid",
+							}
+						}
+					}
+				},
+				Config: providerConfig + `
+					resource "uxi_group" "my_group" {
+						name       = "name"
+						parent_uid = "updated_parent_uid"
+					}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("uxi_group.my_group", "name", "name"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "parent_uid", "updated_parent_uid"),
+					resource.TestCheckResourceAttr("uxi_group.my_group", "id", "new_uid"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
 		},
 	})
 }
