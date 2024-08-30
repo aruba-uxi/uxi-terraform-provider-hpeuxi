7d45676d293e740c6e2c8c2d6a1d38e2d000d423
1riatsila1 <aliyan321@gmail.com>
2024-08-30 16:13:11 +0200

    feat: root group data source

--- pkg/config-api-provider/provider/resources/group.go
+++ pkg/config-api-provider/provider/resources/group.go
@@ -62,7 +62,7 @@ func (r *groupResource) Schema(_ context.Context, _ resource.SchemaRequest, resp
 				Required: true,
 			},
 			"parent_group_id": schema.StringAttribute{
-				Optional: true,
+				Required: true,
 				PlanModifiers: []planmodifier.String{
 					// UXI business logic does not permit moving of groups
 					stringplanmodifier.RequiresReplace(),
--- pkg/config-api-provider/test/utils.go
+++ pkg/config-api-provider/test/utils.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"github.com/aruba-uxi/configuration-api-terraform-provider/pkg/terraform-provider-configuration/provider/data-sources"
 	"github.com/aruba-uxi/configuration-api-terraform-provider/pkg/terraform-provider-configuration/provider/resources"
 )
 
@@ -40,6 +41,15 @@ func GenerateGroupResponseModel(uid string, non_replacement_field_postfix string
 		Name:      "name" + non_replacement_field_postfix,
 		ParentUid: &parent_uid,
 		Path:      parent_uid + "." + uid,
+	}
+}
+
+func GenerateRootGroupResponseModel(uid string) datasources.RootGroupResponseModel {
+	return datasources.RootGroupResponseModel{
+		UID:       uid,
+		Name:      "root",
+		ParentUid: nil,
+		Path:      uid,
 	}
 }
 
