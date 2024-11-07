25f92eaac8e59c6714dc984d8949106f5795a7f6
Alistair Yan <52126234+1riatsila1@users.noreply.github.com>
2024-11-07 12:12:10 +0400

    feat: live acceptance tests for service tests (#67)

---
+++ test/live/resources/service_test.go
@@ -0,0 +1,93 @@
+package resource_test
+
+import (
+	"github.com/aruba-uxi/terraform-provider-configuration/test/live/config"
+	"github.com/aruba-uxi/terraform-provider-configuration/test/live/provider"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+)
+
+func TestServiceTestResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			// we required terraform 1.7.0 and above for the `removed` block
+			tfversion.RequireAbove(tfversion.Version1_7_0),
+		},
+		Steps: []resource.TestStep{
+			// Creating a service_test is not allowed
+			{
+				Config: provider.ProviderConfig + `
+					resource "uxi_service_test" "my_service_test" {
+						name = "` + config.ServiceTestName + `"
+					}`,
+
+				ExpectError: regexp.MustCompile(
+					`(?s)creating a service_test is not supported; service_tests can only be\s*imported`,
+				),
+			},
+			// Importing a service_test
+			{
+				Config: provider.ProviderConfig + `
+					resource "uxi_service_test" "my_service_test" {
+						name = "` + config.ServiceTestName + `"
+					}
+
+					import {
+						to = uxi_service_test.my_service_test
+						id = "` + config.ServiceTestUid + `"
+					}`,
+
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"uxi_service_test.my_service_test",
+						"name",
+						config.ServiceTestName,
+					),
+					resource.TestCheckResourceAttr(
+						"uxi_service_test.my_service_test",
+						"id",
+						config.ServiceTestUid,
+					),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "uxi_service_test.my_service_test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Updating a service_test is not allowed
+			{
+				Config: provider.ProviderConfig + `
+				resource "uxi_service_test" "my_service_test" {
+					name = "` + config.ServiceTestName + `-updated-name"
+				}`,
+				ExpectError: regexp.MustCompile(
+					`(?s)updating a service_test is not supported; service_tests can only be updated\s*through the dashboard`,
+				),
+			},
+			// Deleting a service_test is not allowed
+			{
+				Config: provider.ProviderConfig + ``,
+				ExpectError: regexp.MustCompile(
+					`(?s)deleting a service_test is not supported; service_tests can only removed from\s*state`,
+				),
+			},
+			// Remove service_test from state
+			{
+				Config: provider.ProviderConfig + `
+					removed {
+						from = uxi_service_test.my_service_test
+
+						lifecycle {
+							destroy = false
+						}
+					}`,
+			},
+		},
+	})
+}
