08f7e41f4af75da73535031fdbdff0d6d500069f
1riatsila1 <aliyan321@gmail.com>
2024-11-11 09:19:24 +0400

    some fixes

--- test/live/config/config.go
+++ test/live/config/config.go
@@ -1,8 +1,13 @@
 package config
 
+// These constants are from the customer:
+// Configuration-API Acceptance Testing (844457745a1111ef880836000a52e73e)
+// And therefore the client_id and client_secret used for the acceptance tests must match this
+// customer.
 const WiredNetworkUid = "ethernet-0ee5b46c2ef0"
 const WiredNetworkName = "tf-provider-acceptance-tests-ethernet-0"
 const WirelessNetworkUid = "ssid-bf704ff37dc0"
 const WirelessNetworkName = "tf-provider-acceptance-tests-ssid-0"
 const ServiceTestUid = "6f81e43d-76f1-4a15-aafe-4ce2371d918a"
 const ServiceTestName = "tf-provider-acceptance-test-0"
+const SensorUid = "4b031caf-cea8-411d-8928-79f518163dae"
