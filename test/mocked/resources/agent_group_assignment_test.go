6e45bd51f0c58c56852e225b0c3e1ec83dc47373
Alistair Yan <52126234+1riatsila1@users.noreply.github.com>
2024-11-13 13:38:44 +0400

    feat: agent group assignment delete integration (#98)

--- test/mocked/resources/agent_group_assignment_test.go
+++ test/mocked/resources/agent_group_assignment_test.go
@@ -1,12 +1,15 @@
 package resource_test
 
 import (
-	"github.com/aruba-uxi/terraform-provider-hpeuxi/test/mocked/provider"
-	"github.com/aruba-uxi/terraform-provider-hpeuxi/test/mocked/util"
+	"regexp"
 	"testing"
 
+	"github.com/aruba-uxi/terraform-provider-hpeuxi/test/mocked/provider"
+	"github.com/aruba-uxi/terraform-provider-hpeuxi/test/mocked/util"
 	"github.com/h2non/gock"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/nbio/st"
 )
 
 func TestAgentGroupAssignmentResource(t *testing.T) {
@@ -127,7 +130,7 @@ func TestAgentGroupAssignmentResource(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
-			// Update and Read testing
+			// Update testing
 			{
 				PreConfig: func() {
 					util.MockGetAgent(
@@ -166,6 +169,7 @@ func TestAgentGroupAssignmentResource(t *testing.T) {
 						),
 						2,
 					)
+					util.MockDeleteAgentGroupAssignment("agent_group_assignment_uid", 1)
 
 					// required for creating another group
 					util.MockPostGroup(
@@ -271,7 +275,7 @@ func TestAgentGroupAssignmentResource(t *testing.T) {
 					),
 				),
 			},
-			// Delete testing automatically occurs in TestCase
+			// Delete testing
 			{
 				PreConfig: func() {
 					util.MockGetAgent(
@@ -310,12 +314,615 @@ func TestAgentGroupAssignmentResource(t *testing.T) {
 						),
 						1,
 					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						2,
+					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid_2",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid_2",
+									"_2",
+								),
+							},
+						),
+						1,
+					)
 					util.MockDeleteGroup("group_uid", 1)
 					util.MockDeleteGroup("group_uid_2", 1)
 					util.MockDeleteAgent("agent_uid", 1)
 					util.MockDeleteAgent("agent_uid_2", 1)
+					util.MockDeleteAgentGroupAssignment("agent_group_assignment_uid_2", 1)
 				},
 				Config: provider.ProviderConfig,
+			},
+		},
+	})
+
+	mockOAuth.Mock.Disable()
+}
+
+func TestAgentGroupAssignmentResource429Handling(t *testing.T) {
+	defer gock.Off()
+	mockOAuth := util.MockOAuth()
+	var mock429 *gock.Response
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Creating a agent group assignment
+			{
+				PreConfig: func() {
+					// required for agent import
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						2,
+					)
+
+					// required for agent group assignment create
+					mock429 = gock.New("https://test.api.capenetworks.com").
+						Post("/networking-uxi/v1alpha1/agent-group-assignments").
+						Reply(429).
+						SetHeaders(util.RateLimitingHeaders)
+					util.MockPostGroup(
+						util.GenerateGroupRequestModel("group_uid", "", ""),
+						util.StructToMap(
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						),
+						1,
+					)
+					util.MockGetGroup(
+						"group_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+							},
+						),
+						1,
+					)
+
+					// required for agent group assignment create
+					util.MockPostAgentGroupAssignment(
+						util.GenerateAgentGroupAssignmentRequest("agent_group_assignment_uid", ""),
+						util.GenerateAgentGroupAssignmentResponse("agent_group_assignment_uid", ""),
+						1,
+					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						1,
+					)
+				},
+
+				Config: provider.ProviderConfig + `
+					resource "uxi_group" "my_group" {
+						name            = "name"
+						parent_group_id = "parent_uid"
+					}
+
+					resource "uxi_agent" "my_agent" {
+						name 			= "name"
+						notes 			= "notes"
+						pcap_mode 		= "light"
+					}
+
+					import {
+						to = uxi_agent.my_agent
+						id = "agent_uid"
+					}
+
+					resource "uxi_agent_group_assignment" "my_agent_group_assignment" {
+						agent_id       = uxi_agent.my_agent.id
+						group_id 		= uxi_group.my_group.id
+					}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"uxi_agent_group_assignment.my_agent_group_assignment",
+						"id",
+						"agent_group_assignment_uid",
+					),
+					func(s *terraform.State) error {
+						st.Assert(t, mock429.Mock.Request().Counter, 0)
+						return nil
+					},
+				),
+			},
+			// ImportState testing
+			{
+				PreConfig: func() {
+					mock429 = gock.New("https://test.api.capenetworks.com").
+						Get("/networking-uxi/v1alpha1/agent-group-assignments").
+						Reply(429).
+						SetHeaders(util.RateLimitingHeaders)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						1,
+					)
+				},
+				ResourceName:      "uxi_agent_group_assignment.my_agent_group_assignment",
+				ImportState:       true,
+				ImportStateVerify: true,
+				Check: func(s *terraform.State) error {
+					st.Assert(t, mock429.Mock.Request().Counter, 0)
+					return nil
+				},
+			},
+			// Delete testing
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						1,
+					)
+					util.MockGetGroup(
+						"group_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+							},
+						),
+						1,
+					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						1,
+					)
+					util.MockDeleteGroup("group_uid", 1)
+					util.MockDeleteAgent("agent_uid", 1)
+					mock429 = gock.New("https://test.api.capenetworks.com").
+						Delete("/networking-uxi/v1alpha1/agent-group-assignments/agent_group_assignment_uid").
+						Reply(429).
+						SetHeaders(util.RateLimitingHeaders)
+					util.MockDeleteAgentGroupAssignment("agent_group_assignment_uid", 1)
+				},
+				Config: provider.ProviderConfig,
+				Check: func(s *terraform.State) error {
+					st.Assert(t, mock429.Mock.Request().Counter, 0)
+					return nil
+				},
+			},
+		},
+	})
+
+	mockOAuth.Mock.Disable()
+}
+
+func TestAgentGroupAssignmentResourceHttpErrorHandling(t *testing.T) {
+	defer gock.Off()
+	mockOAuth := util.MockOAuth()
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Creating an agent group assignment - errors
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						2,
+					)
+
+					// required for group create
+					util.MockPostGroup(
+						util.GenerateGroupRequestModel("group_uid", "", ""),
+						util.StructToMap(
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						),
+						1,
+					)
+					util.MockGetGroup("group_uid", util.GeneratePaginatedResponse(
+						[]map[string]interface{}{
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						},
+					),
+						2,
+					)
+
+					// agent group assignment create
+					gock.New("https://test.api.capenetworks.com").
+						Post("/networking-uxi/v1alpha1/agent-group-assignments").
+						Reply(400).
+						JSON(map[string]interface{}{
+							"httpStatusCode": 400,
+							"errorCode":      "HPE_GL_ERROR_BAD_REQUEST",
+							"message":        "Validation error - bad request",
+							"debugId":        "12312-123123-123123-1231212",
+						})
+				},
+
+				Config: provider.ProviderConfig + `
+					resource "uxi_group" "my_group" {
+						name            = "name"
+						parent_group_id = "parent_uid"
+					}
+
+					resource "uxi_agent" "my_agent" {
+						name      = "name"
+						notes 	  = "notes"
+						pcap_mode = "light"
+					}
+
+					import {
+						to = uxi_agent.my_agent
+						id = "agent_uid"
+					}
+
+					resource "uxi_agent_group_assignment" "my_agent_group_assignment" {
+						agent_id = uxi_agent.my_agent.id
+						group_id   = uxi_group.my_group.id
+					}`,
+				ExpectError: regexp.MustCompile(
+					`(?s)Validation error - bad request\s*DebugID: 12312-123123-123123-1231212`,
+				),
+			},
+			// read not found error
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						2,
+					)
+
+					// required for group create
+					util.MockPostGroup(
+						util.GenerateGroupRequestModel("group_uid", "", ""),
+						util.StructToMap(
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						),
+						1,
+					)
+					util.MockGetGroup("group_uid", util.GeneratePaginatedResponse(
+						[]map[string]interface{}{
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						},
+					),
+						2,
+					)
+
+					// agent group assignment read
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse([]map[string]interface{}{}),
+						1,
+					)
+				},
+				Config: provider.ProviderConfig + `
+					resource "uxi_group" "my_group" {
+						name            = "name"
+						parent_group_id = "parent_uid"
+					}
+
+					resource "uxi_agent" "my_agent" {
+						name 	  = "name"
+						notes 	  = "notes"
+						pcap_mode = "light"
+					}
+
+					import {
+						to = uxi_agent.my_agent
+						id = "agent_uid"
+					}
+
+					resource "uxi_agent_group_assignment" "my_agent_group_assignment" {
+						agent_id = uxi_agent.my_agent.id
+						group_id   = uxi_group.my_group.id
+					}
+
+					import {
+						to = uxi_agent_group_assignment.my_agent_group_assignment
+						id = "agent_group_assignment_uid"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`Error: Cannot import non-existent remote object`),
+			},
+			// Read 5xx error
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						2,
+					)
+
+					// required for group create
+					util.MockPostGroup(
+						util.GenerateGroupRequestModel("group_uid", "", ""),
+						util.StructToMap(
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						),
+						1,
+					)
+					util.MockGetGroup("group_uid", util.GeneratePaginatedResponse(
+						[]map[string]interface{}{
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						},
+					),
+						2,
+					)
+
+					// agent group assignment read
+					gock.New("https://test.api.capenetworks.com").
+						Get("/networking-uxi/v1alpha1/agent-group-assignments").
+						Reply(500).
+						JSON(map[string]interface{}{
+							"httpStatusCode": 500,
+							"errorCode":      "HPE_GL_ERROR_INTERNAL_SERVER_ERROR",
+							"message":        "Current request cannot be processed due to unknown issue",
+							"debugId":        "12312-123123-123123-1231212",
+						})
+				},
+				Config: provider.ProviderConfig + `
+					resource "uxi_group" "my_group" {
+						name            = "name"
+						parent_group_id = "parent_uid"
+					}
+
+					resource "uxi_agent" "my_agent" {
+						name 	  = "name"
+						notes 	  = "notes"
+						pcap_mode = "light"
+					}
+
+					import {
+						to = uxi_agent.my_agent
+						id = "agent_uid"
+					}
+
+					resource "uxi_agent_group_assignment" "my_agent_group_assignment" {
+						agent_id = uxi_agent.my_agent.id
+						group_id   = uxi_group.my_group.id
+					}
+
+					import {
+						to = uxi_agent_group_assignment.my_agent_group_assignment
+						id = "agent_group_assignment_uid"
+					}
+				`,
+
+				ExpectError: regexp.MustCompile(
+					`(?s)Current request cannot be processed due to unknown issue\s*DebugID: 12312-123123-123123-1231212`,
+				),
+			},
+			// Actually creating an agent group assignment - for next step
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						2,
+					)
+
+					// required for group create
+					util.MockPostGroup(
+						util.GenerateGroupRequestModel("group_uid", "", ""),
+						util.StructToMap(
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						),
+						1,
+					)
+					util.MockGetGroup("group_uid", util.GeneratePaginatedResponse(
+						[]map[string]interface{}{
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						},
+					),
+						2,
+					)
+
+					// required for agent group assignment create
+					util.MockPostAgentGroupAssignment(
+						util.GenerateAgentGroupAssignmentRequest(
+							"agent_group_assignment_uid",
+							"",
+						),
+						util.GenerateAgentGroupAssignmentResponse(
+							"agent_group_assignment_uid",
+							"",
+						),
+						1,
+					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						1,
+					)
+				},
+
+				Config: provider.ProviderConfig + `
+					resource "uxi_group" "my_group" {
+						name            = "name"
+						parent_group_id = "parent_uid"
+					}
+
+					resource "uxi_agent" "my_agent" {
+						name 	  = "name"
+						notes 	  = "notes"
+						pcap_mode = "light"
+
+					}
+
+					import {
+						to = uxi_agent.my_agent
+						id = "agent_uid"
+					}
+
+					resource "uxi_agent_group_assignment" "my_agent_group_assignment" {
+						agent_id = uxi_agent.my_agent.id
+						group_id = uxi_group.my_group.id
+					}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"uxi_agent_group_assignment.my_agent_group_assignment",
+						"agent_id",
+						"agent_uid",
+					),
+				),
+			},
+			// Delete agent-group assignment and remove agents from state - errors
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						1,
+					)
+					util.MockGetGroup("group_uid", util.GeneratePaginatedResponse(
+						[]map[string]interface{}{
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						},
+					),
+						1,
+					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						1,
+					)
+
+					// agent group assignment create
+					gock.New("https://test.api.capenetworks.com").
+						Delete("/networking-uxi/v1alpha1/agent-group-assignments").
+						Reply(403).
+						JSON(map[string]interface{}{
+							"httpStatusCode": 403,
+							"errorCode":      "HPE_GL_ERROR_FORBIDDEN",
+							"message":        "Forbidden - user has insufficient permissions to complete the request",
+							"debugId":        "12312-123123-123123-1231212",
+						})
+
+				},
+				Config: provider.ProviderConfig + `
+					removed {
+						from = uxi_agent.my_agent
+
+						lifecycle {
+							destroy = false
+						}
+					}`,
+				ExpectError: regexp.MustCompile(
+					`(?s)Forbidden - user has insufficient permissions to complete the request\s*DebugID: 12312-123123-123123-1231212`,
+				),
+			},
+			// Actually delete agent-group assignment and remove agents from state
+			{
+				PreConfig: func() {
+					util.MockGetAgent(
+						"agent_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentResponseModel("agent_uid", ""),
+							},
+						),
+						1,
+					)
+					util.MockGetGroup("group_uid", util.GeneratePaginatedResponse(
+						[]map[string]interface{}{
+							util.GenerateNonRootGroupResponseModel("group_uid", "", ""),
+						},
+					),
+						1,
+					)
+					util.MockGetAgentGroupAssignment(
+						"agent_group_assignment_uid",
+						util.GeneratePaginatedResponse(
+							[]map[string]interface{}{
+								util.GenerateAgentGroupAssignmentResponse(
+									"agent_group_assignment_uid",
+									"",
+								),
+							},
+						),
+						1,
+					)
+					util.MockDeleteGroup("group_uid", 1)
+					util.MockDeleteAgentGroupAssignment("agent_group_assignment_uid", 1)
+				},
+				Config: provider.ProviderConfig + `
+					removed {
+						from = uxi_agent.my_agent
+
+						lifecycle {
+							destroy = false
+						}
+					}`,
 			},
 		},
 	})
