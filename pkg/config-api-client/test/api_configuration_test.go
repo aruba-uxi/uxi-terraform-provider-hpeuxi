0e9f154b5cdfaea6242a680045dc4282ea6b4c08
1riatsila1 <aliyan321@gmail.com>
2024-11-14 14:13:13 +0400

    sort of start fixing the tests

--- pkg/config-api-client/test/api_configuration_test.go
+++ pkg/config-api-client/test/api_configuration_test.go
@@ -97,7 +97,7 @@ func TestConfigurationAPI(t *testing.T) {
 			)
 		name := "new_name"
 		notes := "new_notes"
-		pcapMode := "off"
+		pcapMode := config_api_client.OFF
 		agentsPatchRequest := config_api_client.AgentsPatchRequest{
 			Name:     &name,
 			Notes:    &notes,
@@ -275,7 +275,7 @@ func TestConfigurationAPI(t *testing.T) {
 						"longitude":          0.0,
 						"latitude":           0.0,
 						"notes":              "notes",
-						"pcapMode":           "pcap_mode",
+						"pcapMode":           "light",
 						"type":               "networking-uxi/sensor",
 					},
 				},
@@ -296,7 +296,7 @@ func TestConfigurationAPI(t *testing.T) {
 		var Longitude float32 = 0.0
 		var Latitude float32 = 0.0
 		Notes := "notes"
-		PcapMode := "pcap_mode"
+		pcapMode := config_api_client.LIGHT
 
 		require.Nil(t, err)
 		assert.Equal(t, http.StatusOK, httpRes.StatusCode)
@@ -313,7 +313,7 @@ func TestConfigurationAPI(t *testing.T) {
 					Longitude:          *config_api_client.NewNullableFloat32(&Longitude),
 					Latitude:           *config_api_client.NewNullableFloat32(&Latitude),
 					Notes:              *config_api_client.NewNullableString(&Notes),
-					PcapMode:           *config_api_client.NewNullableString(&PcapMode),
+					PcapMode:           *config_api_client.NewNullablePcapMode(&pcapMode),
 					Type:               "networking-uxi/sensor",
 				},
 			},
