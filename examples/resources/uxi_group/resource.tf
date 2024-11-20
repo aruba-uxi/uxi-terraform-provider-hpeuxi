53b3e006670c517a34ba83c893ba24aff5369c93
cliftonHPE <clifton.dhanee@hpe.com>
2024-11-20 13:23:02 +0200

    add comments

--- examples/resources/uxi_group/resource.tf
+++ examples/resources/uxi_group/resource.tf
@@ -1,7 +1,9 @@
+# Create a root group (no parent_group_id)
 resource "uxi_group" "parent_group" {
   name            = "Parent Group"
 }
 
+# Create a child group of the root group
 resource "uxi_group" "child_group" {
   name            = "Child Group"
   parent_group_id = uxi_group.parent_group.id
