1f3ed303ee0e401cf54d06fa68ec804b7bdeaff7
cliftonHPE <clifton.dhanee@hpe.com>
2024-11-21 13:37:37 +0200

    update comments

--- examples/resources/uxi_group/resource.tf
+++ examples/resources/uxi_group/resource.tf
@@ -1,9 +1,9 @@
-# Create a parent group attached to the root node
+# Create level 1 group attached to the root node
 resource "uxi_group" "level_1" {
   name            = "Parent Group"
 }
 
-# Create a child group attached to the parent group
+# Create level 2 group attached to level 1 group
 resource "uxi_group" "level_2" {
   name            = "Child Group"
   parent_group_id = uxi_group.level_1.id
