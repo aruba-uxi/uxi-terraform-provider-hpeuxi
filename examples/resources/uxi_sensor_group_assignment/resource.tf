25e9866f44be01e70e681ead979800f4abd07898
cliftonHPE <clifton.dhanee@hpe.com>
2024-11-26 14:45:39 +0200

    remove test config

--- examples/resources/uxi_sensor_group_assignment/resource.tf
+++ examples/resources/uxi_sensor_group_assignment/resource.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    uxi = {
-      source = "registry.terraform.io/arubauxi/hpeuxi"
-    }
-  }
-}
-
-provider "uxi" {
-
-}
-
 resource "uxi_sensor_group_assignment" "my_sensor_group_assignment" {
   sensor_id = "<my_sensor_id>"
   group_id = "<my_group_id>"
