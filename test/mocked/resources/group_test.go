144356505aed6d5f33284f44fd56c0ed63841a7f
Sias Mey <siasmey@gmail.com>
2024-12-04 15:09:20 +0200

    test: slim down extra steps in groups resource tests

--- test/mocked/resources/group_test.go
+++ test/mocked/resources/group_test.go
@@ -56,83 +56,15 @@ func Test_CreateGroupResource(t *testing.T) {
 					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "id", "id"),
 				),
 			},
-			// Update that does not trigger a recreate
-			{
-				PreConfig: func() {
-					// existing group
-					util.MockGetGroup("id", util.GenerateGroupGetResponse("id", "", ""), 1)
-					// updated group
-					util.MockPatchGroup(
-						"id",
-						util.GenerateGroupPatchRequest("_2"),
-						util.GenerateGroupPatchResponse("id", "_2", ""),
-						1,
-					)
-					util.MockGetGroup("id", util.GenerateGroupGetResponse("id", "_2", ""), 3)
-					util.MockGetGroup(
-						"parent_id",
-						util.GenerateGroupGetResponse("parent_id", "", ""),
-						1,
-					)
-				},
-				Config: provider.ProviderConfig + `
-					resource "hpeuxi_group" "my_group" {
-						name            = "name_2"
-						parent_group_id = "parent_id"
-					}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "name", "name_2"),
-					resource.TestCheckResourceAttr(
-						"hpeuxi_group.my_group",
-						"parent_group_id",
-						"parent_id",
-					),
-					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "id", "id"),
-				),
-			},
-			// Update that does trigger a recreate
-			{
-				PreConfig: func() {
-					// existing group
-					util.MockGetGroup("id", util.GenerateGroupGetResponse("id", "", ""), 1)
-					// new group (replacement)
-					util.MockPostGroup(
-						util.GenerateNonRootGroupPostRequest("new_id", "", "_2"),
-						util.GenerateGroupPostResponse("new_id", "", "_2"),
-						1,
-					)
-					util.MockGetGroup(
-						"new_id",
-						util.GenerateGroupGetResponse("new_id", "", "_2"),
-						1,
-					)
-					// delete old group (being replaced)
-					util.MockDeleteGroup("id", 1)
-				},
-				Config: provider.ProviderConfig + `
-					resource "hpeuxi_group" "my_group" {
-						name            = "name"
-						parent_group_id = "parent_id_2"
-					}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "name", "name"),
-					resource.TestCheckResourceAttr(
-						"hpeuxi_group.my_group",
-						"parent_group_id",
-						"parent_id_2",
-					),
-					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "id", "new_id"),
-				),
-			},
 			// Delete
 			{
 				PreConfig: func() {
 					util.MockGetGroup(
-						"new_id",
-						util.GenerateGroupGetResponse("new_id", "", "_2"),
+						"id",
+						util.GenerateGroupGetResponse("id", "", ""),
 						1,
 					)
-					util.MockDeleteGroup("new_id", 1)
+					util.MockDeleteGroup("id", 1)
 				},
 				Config: provider.ProviderConfig,
 			},
@@ -313,15 +245,6 @@ func Test_UpdateGroupResource_WithoutRecreate_ShouldSucceed(t *testing.T) {
 					name            = "name"
 					parent_group_id = "parent_id"
 				}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "name", "name"),
-					resource.TestCheckResourceAttr(
-						"hpeuxi_group.my_group",
-						"parent_group_id",
-						"parent_id",
-					),
-					resource.TestCheckResourceAttr("hpeuxi_group.my_group", "id", "id"),
-				),
 			},
 			// Update that does not trigger a recreate
 			{
